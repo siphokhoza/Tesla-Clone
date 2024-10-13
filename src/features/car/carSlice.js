f6d75ac09b33111017bf008d6a7467ba121e679f
siphokhoza <2680877@studemts.wits.ac.za>
2024-10-13 03:01:01 +0200

    Initial commit

--- src/features/car/carSlice.js
+++ src/features/car/carSlice.js
@@ -10,6 +10,6 @@ const carSlice = createSlice({
     reducers: {}
 })
 
-export const selectCars = state => state.cars
+export const selectCars = state => state.car.cars
 
 export default carSlice.reducer
